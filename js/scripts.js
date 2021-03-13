1116555c444f469e7857c79307b700d3ae78f3d7
Todd Routhier <fonemasta@gmail.com>
2021-03-13 11:47:14 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,12 @@ $(function () {
         }
     })
 
+    $('#reserveButton').click(function(){
+        $('#reserveModal').modal("show");
+    })
+
+    $('#loginButton').click(function(){
+        $('#loginModal').modal("show");
+    })
+
 })
